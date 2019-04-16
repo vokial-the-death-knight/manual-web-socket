6fee6472c1b03d3d368ea1b667305ddcbfc90c98
Baal Cadar <baal-cadar@swamp.camp>
2019-04-16 23:20:33 +0200

    getScript is function now

--- build-wrapper.js
+++ build-wrapper.js
@@ -14,7 +14,8 @@ if (!fs.existsSync(path.resolve(__dirname, "dist"))) {
   fs.mkdirSync(path.resolve(__dirname, "dist"));
 }
 
-const template = "module.exports = { getScript: `" + script + "` }";
+const template =
+  "module.exports = { getScript: function(){ return `" + script + "`} }";
 
 fs.writeFileSync(
   path.resolve(__dirname, "dist", "manual-web-socket.js"),
